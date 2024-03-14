9cd722b2ab39e4268b3c356f0d0d70e78c7bba1d
Nuno Gonçalves <nuno.gonkalves@gmail.com>
2024-03-14 21:30:31 +0000

    custom plugins and configuration

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,14 @@
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+--
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+}
